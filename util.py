dc0eb5dbfe6bb0a1fa3287edd3c15bceebafac14
ocatak <f.ozgur.catak@gmail.com>
2021-07-04 20:04:18 +0200

    PURE-commit

---
+++ util.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  4 19:56:47 2021
+
+@author: ozgur
+"""
+import os,sys,glob,ntpath
+import pandas as pd
+ROOT_DIR = os.path.abspath("ssd_keras")
+sys.path.append(ROOT_DIR)  # To find local version of the library
