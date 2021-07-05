83f909a8131160967423dc153c02fdc73ec8e4f9
ocatak <f.ozgur.catak@gmail.com>
2021-07-05 20:46:51 +0200

    PURE-commit

--- util.py
+++ util.py
@@ -10,8 +10,16 @@ import sys
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
 from keras.optimizers import Adam
-
 from keras import backend as K
+import itertools
+import pandas as pd
+from imageio import imread
+from keras.preprocessing import image
+import numpy as np
+from matplotlib.patches import Rectangle
+from sklearn.cluster import DBSCAN
+from scipy.spatial import ConvexHull
+from matplotlib import pyplot as plt
 
 from models.keras_ssd300 import ssd_300 # pylint: disable=import-error
 from keras_loss_function.keras_ssd_loss import SSDLoss # pylint: disable=import-error
@@ -28,6 +36,7 @@ classes = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
 # Set the image size.
 IMG_HEIGHT = 300
 IMG_WIDTH = 300
+COLOR_CODES = itertools.cycle(('r','g','b','c','m','y','k'))
 
 def __download_model():
     gdd.download_file_from_google_drive(file_id='19FH-pBzYPCbPi7jxRJSB-Y9YIwAYGOE1',
@@ -75,5 +84,90 @@ def get_model(p_size,mc_dropout=True):
     model.compile(optimizer=adam, loss=ssd_loss.compute_loss)
     return model
 
+def get_pred_uncertainty(fname, model, T=20, 
+                         plot_ground_truth=False, mc_dropout=True):
+    ground_truth_file = fname + '.txt'
+    ground_truth = pd.read_csv(ground_truth_file,
+                               names=['class','x1','y1','x2','y2'],
+                               sep=' ')
+    
+    orig_images = [] # Store the images here.
+    input_images = [] # Store resized versions of the images here.
+
+    orig_images.append(imread(fname))
+    img = image.load_img(fname, target_size=(IMG_HEIGHT, IMG_WIDTH))
+    img = image.img_to_array(img) 
+    input_images.append(img)
+    input_images = np.array(input_images)
+    
+    mc_locations = []
+
+    for _ in range(T):
+        y_pred = model.predict(input_images)
+        confidence_threshold = 0.5
+        y_pred_thresh = [y_pred[k][y_pred[k,:,1] > confidence_threshold] for k in range(y_pred.shape[0])]
+        
+        for box in y_pred_thresh[0]:
+            x1 = box[2] * orig_images[0].shape[1] / IMG_WIDTH
+            y1 = box[3] * orig_images[0].shape[0] / IMG_HEIGHT
+            x2 = box[4] * orig_images[0].shape[1] / IMG_WIDTH
+            y2 = box[5] * orig_images[0].shape[0] / IMG_HEIGHT
+            width, height = x2 - x1, y2 - y1
+            mc_locations.append(np.array([x1,y1,x2,y2,x1+width/2,y1+height/2]))
+            
+    mc_locations = np.array(mc_locations)
+    avg_surface = -1.0
+    if mc_locations.shape[0]:
+        clustering = DBSCAN(eps=100, min_samples=2).fit(mc_locations)
+        mc_locations = np.c_[mc_locations,clustering.labels_.ravel()]
+        
+        mc_locations_df = pd.DataFrame(data=mc_locations, columns=['x1','y1','x2','y2','center_x','center_y','label'])
+        cluster_labels = np.unique(mc_locations[:,6])
+        total_cluster_surface = 0.0
+        if mc_dropout == True:
+            for cluster_label in cluster_labels:
+                cluster_df = mc_locations_df.query('label == ' + str(cluster_label))
+                if cluster_df.shape[0] > 2:
+                    center_data = cluster_df[['x1','y1']].values
+                    hull = ConvexHull(center_data)
+                    total_cluster_surface += hull.area
+                    
+                    center_data = cluster_df[['x2','y1']].values
+                    hull = ConvexHull(center_data)
+                    total_cluster_surface += hull.area
+                    
+                    center_data = cluster_df[['x1','y2']].values
+                    hull = ConvexHull(center_data)
+                    total_cluster_surface += hull.area
+                    
+                    center_data = cluster_df[['x2','y2']].values
+                    hull = ConvexHull(center_data)
+                    total_cluster_surface += hull.area
+                avg_surface = total_cluster_surface/mc_locations.shape[0]
+    
+    if plot_ground_truth:
+        data = plt.imread(fname)
+        fig, ax = plt.subplots(1,1,sharey=True, figsize=(12,5))
+        ax.imshow(data)
+        for i in range(mc_locations.shape[0]):
+            x1, y1, x2, y2 = mc_locations[i,0:4]
+            width, height = x2 - x1, y2 - y1
+            rect = Rectangle((x1, y1), width, height, fill=False, color='red')
+            ax.add_patch(rect)
+            ax.scatter(mc_locations[i,4],mc_locations[i,5], marker='x',
+                       c='g',s=150)
+            
+        for index, row in ground_truth.iterrows():
+            x1, y1, x2, y2 = row['x1'], row['y1'], row['x2'],row['y2']
+            width, height = x2 - x1, y2 - y1
+            rect = Rectangle((x1, y1), width, height, fill=False, 
+                             color='white',lw=3,ls='--')
+            ax.add_patch(rect)
+        plt.show()
+    
+    return mc_locations, avg_surface
+
 if __name__ == "__main__":
-    tmp_model = get_model(0.2)
+    tmp_model = get_model(0.05)
+    get_pred_uncertainty('images/Stanford/00002.jpg',tmp_model, T=10, 
+                         plot_ground_truth=True)
